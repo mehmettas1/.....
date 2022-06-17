743c73cc43f4a1df25968ca3890af1f8092fe491
mehmet <tas349873@gmail.com>
2022-06-17 17:40:03 +0300

    v4

--- app.js
+++ app.js
@@ -6,10 +6,19 @@ button.addEventListener("click",()=>{
     const getPersonData = async()=> {
         try {
             const url = "https://randomuser.me/api"
-            let res = await fetch(url)
-            let data = await res.json()
+            let res = await fetch(url);
+            let data = await res.json();
+            console.log(data);
+        
+            createPersonData(data);
+
+
         } catch (error) {
-            
+            alert(error);
         }
     }
+    createPersonData = (data) => {
+        let name =
+    };
+    getPersonData();
 })
