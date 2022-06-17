e8279fdfae68c8e59892597cb09114917b9fd3b5
mehmet <tas349873@gmail.com>
2022-06-17 17:52:49 +0300

    v5

--- app.js
+++ app.js
@@ -18,7 +18,41 @@ button.addEventListener("click",()=>{
         }
     }
     createPersonData = (data) => {
-        let name =
+        let {first,last}=data.results[0].name;
+        let location =data.results[0].location.country;
+        let {email,phone} = data.results[0];
+        let image = data.results[0].picture.medium;
+        // let name = data.results[0]
+        // console.log(first);
+        // console.log(last);
+        // console.log(location);
+        // console.log(email);
+        // console.log(phone);
+
+
+        innerContainer.innerHTML = `
+        <img class="image" src="person-0.jpg" alt="" />
+         <p>
+            <i class="fa-solid fa-user"></i> Name :
+            <span class="name"> Anderson</span>
+        </p>
+        <p>
+            <i class="fa-solid fa-user"></i> LastName :
+            <span class="last-name"> Manderson </span>
+        </p>
+        <p>
+            <i class="fa-solid fa-magnifying-glass-location"></i> Location :
+            <span class="location"> Adana</span>
+        </p>
+        <p>
+            <i class="fa-solid fa-phone-flip"></i>Phone :
+            <span class="phone"> 123-456-789</span>
+        </p>
+        <p>
+            <i class="fa-solid fa-envelope"></i>Email :
+            <span class="email"> asdfg@gmail.com</span>
+        </p>`
+
     };
     getPersonData();
-})
+});
